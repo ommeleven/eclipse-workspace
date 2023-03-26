1ec7e523f3f28291043c430794b82ff827840fae
ommeleven <dangeom94@gmail.com>
2023-03-25 19:33:24 -0500

    test

--- LearningJava/Simple.java
+++
@@ -1,9 +0,0 @@
-import java.io.*;
-class Simple{
-
-    public static void main(String args[]){
-
-        System.out.println("Hellow");
-    }
-
-}
--- LearningJava/src/Day10/throwsException.java
+++ LearningJava/src/Day10/throwsException.java
@@ -2,12 +2,8 @@ package Day10;
 
 public class throwsException {
 
-	public static void main(String[] args)  {
-		int a = 199;
-		System.out.println(a/0);
-
-
-
+	public static void main(String[] args) {
+		
 
 	}
 
